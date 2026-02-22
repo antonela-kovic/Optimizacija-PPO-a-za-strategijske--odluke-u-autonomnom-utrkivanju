fdb60862490c8425533d08a277e127d3e2a19246
Antonela Ković <115999525+antonela-kovic@users.noreply.github.com>
2026-02-22 14:47:49 +0100

    Remove unused function and retain feature loading
    Removed unused generate_weather_sequence function and retained feature selection and loading functions.

--- utils.py
+++ utils.py
@@ -1,18 +1,6 @@
 # === utils.py ===
 import numpy as np
 
-# Može se ukloniti ova funkcija(generate_weather_sequence) jer je trenutno train.py prešao na Weather 
-# kao značajku u prepare_data.py te se više ne koristi u f1_env.py.
-
-# def generate_weather_sequence(num_laps):
-#     weather = [1]
-#     for _ in range(1, num_laps):
-#         last = weather[-1]
-#         change = np.random.choice([-1, 0, 1], p=[0.1, 0.8, 0.1])
-#         new = min(max(last + change, 1), 3)
-#         weather.append(new)
-#     return weather
-
 # === utils.py ===
 selected_features = [
     "TyreLife", "CompoundEncoded", "LapNumber", "TrackStatus",
@@ -28,3 +16,4 @@ def load_obs_array(path="data/obs_array.npy"):
 
 def load_true_actions(path="data/true_actions.npy"):
     return np.load(path)
+
